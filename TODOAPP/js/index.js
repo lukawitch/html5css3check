2d6a2ba66673cd8feeca1d873f47b6b14875b816
lukawitch <lukawitch@naver.com>
2019-09-09 09:46:24 +0900

    commit

---
+++ TODOAPP/js/index.js
@@ -0,0 +1,23 @@
+function add(){
+    var todo=document.getElementById("doit").value;
+    var list= document.getElementById("list");
+   var li=document.createElement(li);
+   //var add="<span class='text'>"+todo+"</span><span class='del'>X</span>"
+   list.innerHTML=list.innerHTML+" <li onclick='checked("+list.getElementsByTagName("li").length+")'; id='del"+list.getElementsByTagName("li").length+"'><span class='text'>"+todo+"</span><span class='del' onclick='del("+list.getElementsByTagName("li").length+")'>X</span></li>";
+   document.getElementById("doit").value=" ";
+   // list.innerHTML=list.innerHTML+" <li ></span>";
+
+}
+
+function del(num){
+var a="del"+num;
+var li=document.getElementById(a);
+li.style.display="none";
+
+}
+
+function checked(num){
+    var a="del"+num;
+    var li=document.getElementById(a);
+    li.classList.add('chk');
+}
